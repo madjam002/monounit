efd2f20476dc7ff6115da44118bbdd0bf58d4d0d
Jamie Greeff <madjam002@yahoo.co.uk>
2013-12-13 23:59:03 +0000

    Updated AssemblyInfo for MonoUnit.CLI

--- src/MonoUnit.CLI/Properties/AssemblyInfo.cs
+++ src/MonoUnit.CLI/Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle("MonoUnit.Console")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MonoUnit Test Runner")]
+[assembly: AssemblyDescription("MonoUnit Test Framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("jamie")]
+[assembly: AssemblyProduct("MonoUnit")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
