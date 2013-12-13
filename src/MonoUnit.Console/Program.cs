7a6d724ee650c4b7001b8beed8375ec322b865cf
Jamie Greeff <madjam002@yahoo.co.uk>
2013-12-13 23:54:09 +0000

    Added MonoUnit.Console project

---
+++ src/MonoUnit.Console/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MonoUnit.Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
