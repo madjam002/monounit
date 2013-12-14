8ad9bdfbf5c056218e83af2af2adbda174dd4cc3
Jamie Greeff <madjam002@yahoo.co.uk>
2013-12-14 14:03:30 +0000

    A directory path can now be given to the CLI for testing All assemblies inside the given path will be tested

--- src/MonoUnit.CLI/AssemblyLoader.cs
+++ src/MonoUnit.CLI/AssemblyLoader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using System.IO;
 
 namespace MonoUnit.CLI
 {
@@ -14,7 +15,37 @@ namespace MonoUnit.CLI
 
         public void Load(string path)
         {
-            Assembly assembly = Assembly.LoadFile(path);
+            // Check if given path is a directory
+            if (Directory.Exists(path))
+            {
+                // Get assemblies in directory
+                var files = Directory.GetFiles(path, "*.dll");
+                foreach (string file in files)
+                {
+                    Load(file);
+                }
+
+                return;
+            }
+
+            // Load assembly
+            Assembly assembly;
+
+            try
+            {
+                assembly = Assembly.LoadFile(path);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Assembly {0} could not be found", path);
+                return;
+            }
+            catch (BadImageFormatException)
+            {
+                Console.WriteLine("Assembly {0} is not a valid assembly", path);
+                return;
+            }
+
             Type testCaseType = typeof(TestCase);
 
             // Find classes in assembly which are test classes
