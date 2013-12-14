d07269892cac726c8b7999bf791ac0f65c33c213
Jamie Greeff <madjam002@yahoo.co.uk>
2013-12-14 02:28:41 +0000

    Started to add MonoUnit.Test tests

---
+++ tests/MonoUnit.Test/SuiteTest.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MonoUnit.Test
+{
+    public class SuiteTest : TestCase
+    {
+        public SuiteTest()
+        {
+            describe("Initialisation", () => {
+
+                it("should initialise suites and specs", () => {
+
+                });
+
+            });
+        }
+    }
+}
+
