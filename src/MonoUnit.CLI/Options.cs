603c8f184fb30b4adb2ee2be44edd09202ab88fa
Jamie Greeff <madjam002@yahoo.co.uk>
2013-12-15 23:47:49 +0000

    Added watch argument to CLI options

--- src/MonoUnit.CLI/Options.cs
+++ src/MonoUnit.CLI/Options.cs
@@ -11,6 +11,9 @@ namespace MonoUnit.CLI
 
         [Option('r', "reporter", DefaultValue = "dot", HelpText = "Reporter to use")]
         public string Reporter { get; set; }
+
+        [Option('w', "watch", DefaultValue = false, HelpText = "Watch the filesystem for changes")]
+        public bool Watch { get; set; }
     }
 }
 
